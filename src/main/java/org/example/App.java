408e202ad0507d4fb063faf2603bb0a583825865
Panyin <peedata@gmail.com>
2024-04-09 15:17:41 -0500

    Initial commit 3

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    @Test()
+    public void testCreateUser() {
+    }
+}
